d366b55c00e956b9f6fa7aef1ea6400f5151d29d
Evgeniy-cmd <evgermol1732@gmail.com>
2021-03-23 12:01:25 +0300

    make-request

--- src/userAPI.js
+++ src/userAPI.js
@@ -1,35 +1,35 @@
 import axios from 'axios'
 
-export const newTask = async() => {
+export const newTask = async(userId) => {
     try {
-        return await axios.post('https://todo-api-learning.herokuapp.com/v1/task/2')
+        return await axios.post(`https://todo-api-learning.herokuapp.com/v1/task/${userId}`)
     }
     catch(error) {
         console.log(error)
     }       
 }    
 
-export const getTask = async() => {
+export const getTask = async(userId) => {
     try {
-        return await axios.get('https://todo-api-learning.herokuapp.com/v1/tasks/2')
+        return await axios.get(`https://todo-api-learning.herokuapp.com/v1/tasks/${userId}`)
     }
     catch(error) {
         console.log(error)
     }
 }
 
-export const checkedTask = async() => {
+export const checkedTask = async(userId, uuid) => {
     try{
-        return await axios.patch('https://todo-api-learning.herokuapp.com/v1/task/2/90b52d7a-3447-48ac-ac7d-e34085c42e23')
+        return await axios.patch(`https://todo-api-learning.herokuapp.com/v1/task/${userId}/${uuid}`)
     }
     catch(error) {
         console.log(error)
     }
 }
 
-export const deleteTask = async() => {
+export const deleteTask = async(userId, uuid) => {
     try {
-        return await axios.delete('https://todo-api-learning.herokuapp.com/v1/task/2/90b52d7a-3447-48ac-ac7d-e34085c42e23')
+        return await axios.delete(`https://todo-api-learning.herokuapp.com/v1/task/${userId}/${uuid}`)
     }
     catch(error) {
         console.log(error)
